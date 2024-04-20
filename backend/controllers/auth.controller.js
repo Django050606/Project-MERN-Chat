c6ffe758a49c8f16b32701fe3e5d061531816944
Adil Yerzhanov <adil.erzhanov1990@gmail.com>
2024-04-21 00:41:55 +0200

    Second try

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+ export const signup=(req,res)=>{
+    Console.log("signupUser");
+ }
+ 
+ export const login=(req,res)=>{
+    Console.log("loginUser");
+ }
+ 
+ export const logout=(req,res)=>{
+    Console.log("logoutUser");
+ }
