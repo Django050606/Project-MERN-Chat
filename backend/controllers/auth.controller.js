9f0c7d7c9fad5efb052e1c4a32a41a8ab05a0db8
Adil Yerzhanov <adil.erzhanov1990@gmail.com>
2024-04-22 00:39:45 +0200

    UserSchema, Profie values

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,33 @@
+ import User from '../models/user.model.js';
+ 
+ 
  export const signup=async(req,res)=>{
     try{
       const {fullName,userName,password,confirmPassword,gender}=req.body;
-    }catch(error){}
+      
+      if(password!= confirmPassword){
+         return req.status(400).json({error:"Passwords dont match!"})
+      }
+   
+      const user = await User.findOne({username});
+
+      if (user){
+         return res.status(400).json({error:"Username already taken!"})
+      }
+   
+      const boyProfilePic='https://avatar.iran.liara.run/public/boy?username=${username}'
+      const girlProfilePic='https://avatar.iran.liara.run/public/girl?username=${username}'
+   
+      const newUser=new User({
+         fullName,
+         username,
+         password,
+         gender,
+         profilePic: gender === "male" ? boyProfilePic:girlProfilePic
+
+      });
+      
+   }catch(error){}
  };
  
  export const login=(req,res)=>{
